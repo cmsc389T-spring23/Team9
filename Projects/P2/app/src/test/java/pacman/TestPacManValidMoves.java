dc74e07c2264ed5be4eef28b14ba67faa0fb7711
Brayden Ngo <bngo1@terpmail.umd.edu>
2023-02-24 13:38:21 -0500

    mainly test edit

--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -9,10 +9,9 @@ public class TestPacManValidMoves extends TestCase {
   public void testPacManValidMoves() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
     Ghost ghost = frame.addGhost(new Location(9,11), "binky", Color.red);	
-		//PacMan pacman = new PacMan("bob", new Location(9,12), new Map(1));
-    PacMan pacman = frame.addPacMan(new Location(9,12));
-		ArrayList<Location> loc = new ArrayList<Location>();
-    //pacman.get_valid_moves();
+		PacMan pacman = new PacMan("bob", new Location(9,12), new Map(1));
+		ArrayList<Location> locations = new ArrayList<Location>();
+    //locations = pacman.get_valid_moves();
     return;
   }
 }
