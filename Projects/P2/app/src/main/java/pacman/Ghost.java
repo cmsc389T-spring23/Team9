e17e598eafd6942298a5dbd93d852d3907497266
Brayden Ngo <bngo1@terpmail.umd.edu>
2023-02-24 12:29:10 -0500

    modified ghost move and added test

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -1,6 +1,7 @@
 package pacman;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 
 public class Ghost {
   String myName;
@@ -18,7 +19,21 @@ public class Ghost {
   }
 
   public boolean move() {
-    return false;
+    // get valid moves
+    ArrayList<Location> validMoves = get_valid_moves();
+
+
+    // legality check
+    if(validMoves == null || validMoves.size() == 0){
+      return false;
+    }
+
+    // choose random location
+    Random rand = new Random();
+    int randomLoc = rand.nextInt(validMoves.size());
+    Location chosenLoc = validMoves.get(randomLoc);
+    this.myLoc = chosenLoc;
+    return true;
   }
 
   public boolean is_pacman_in_range() {
