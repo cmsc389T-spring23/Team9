07bbb9f738e6924158c73a2fe4813e57ae6b1208
Brayden Ngo <bngo1@terpmail.umd.edu>
2023-03-01 18:26:13 -0500

    fixes to method and test

--- Projects/P2/app/src/main/java/pacman/PacMan.java
+++ Projects/P2/app/src/main/java/pacman/PacMan.java
@@ -17,14 +17,22 @@ public class PacMan {
 
   public ArrayList<Location> get_valid_moves() {
     ArrayList<Location> validMovesList = new ArrayList<Location>();
-    for(int i = 0; i < 2; i++){
-      for(int j = 0; j < 2; j++){
-        if(myMap.getLoc(new Location(i,j)).contains(Map.Type.WALL) == false){
+    int iMax = this.myLoc.x + 2;
+    int jMax = this.myLoc.y + 2;
+
+    for(int i = this.myLoc.x; i < iMax; i++){
+      for(int j = this.myLoc.y; j < jMax; j++){
+        
+        // first check is because we need getLoc to get implemented (otherwise gradle gives null pointer exception)
+        if(myMap.getLoc(new Location(i,j)) == null || myMap.getLoc(new Location(i,j)).contains(Map.Type.WALL)){
           validMovesList.add(new Location(i,j));
-        }
+          System.out.println(i);
+          System.out.println(j);
+        }  
       }
     }
-
+    // remove the position pacman is already at
+    validMovesList.remove(0);
     return validMovesList;
   }
 
--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -8,10 +8,14 @@ public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
-    Ghost ghost = frame.addGhost(new Location(9,11), "binky", Color.red);	
-		PacMan pacman = new PacMan("bob", new Location(9,12), new Map(1));
-		ArrayList<Location> locations = new ArrayList<Location>();
-    //locations = pacman.get_valid_moves();
-    return;
+    Ghost ghost = frame.addGhost(new Location(9,15), "binky", Color.red);	
+		PacMan pacman = new PacMan("bob", new Location(9,11), new Map(5));
+		ArrayList<Location> locations = pacman.get_valid_moves();
+
+    ArrayList<Location> answers = new ArrayList<Location>();
+    answers.add(new Location(9,12));
+    answers.add(new Location(10,11));
+    answers.add(new Location(10,12));
+    assertEquals(locations, answers);
   }
 }
