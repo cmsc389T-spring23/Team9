7411fd7f112e4d43cb5774ed49ae075f262eec70
Rohit Sengupta <rohitrupsengupta12@gmail.com>
2023-03-01 19:58:54 -0500

    Revert "Pacman is ghost in range"

--- Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManConsume.java
@@ -6,8 +6,6 @@ import java.awt.Color;
 public class TestPacManConsume extends TestCase {
 
   public void testPacManConsume() throws FileNotFoundException {
-	 NoFrame frame = new NoFrame();
-    PacMan pacman = frame.addPacMan(new Location(9, 11));
-    assertTrue(pacman.consume() == null);
+    return;
   }
 }
