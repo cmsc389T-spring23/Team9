300c1cf9d9285bdf5a87a29b6ad1d7bd1af73c04
Rohit Sengupta <rohitsengupta@rohits-mbp-2.mynetworksettings.com>
2023-03-01 21:25:15 -0500

    changes to ghost.attack() and test - Rohit

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,6 +6,11 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
-    return;
+    Map newMap = new Map(9);
+    Ghost ghost = new Ghost("ghost", new Location(1,2), newMap);
+    newMap.add("pacman",new Location(1,1),new PacManComponent(1,1,1),Map.Type.PACMAN);
+    newMap.add("ghost",new Location(1,2),new CookieComponent(1,1,1),Map.Type.GHOST);
+    //ghost.attack();
+    
   }
 }
